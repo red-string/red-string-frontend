fe4552fe21d8bd20f23f7cb26383978d7a1f6f35
Nick Nauert <nicknauert@gmail.com>
2017-10-18 14:17:51 -0400

    Graph mapping function prototypes

---
+++ src/components/StringMap.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import { InteractiveForceGraph, ForceGraph, ForceGraphNode, ForceGraphLink } from 'react-vis-force';
+
+
+export default class StringMap extends Component {
+
+    createFileNodes = (props) => {
+        return props.files.map((file)=> {
+            return <ForceGraphNode showLabel key={file.file_name + "fkey"} node={{ id: file.file_name, data: file.file_name }} fill="white" stroke="black" r="25" />
+        })
+    }
+
+    createTagNodes = (props) => {
+        return props.tags.map((tag)=>{
+            return <ForceGraphNode showLabel key={tag.tag_name + "tkey"} node={{ id: tag.tag_name, data: tag.tag_name }} fill="white" stroke="black" r="25" />
+        })
+    }
+
+    createLinks = (props) => {
+        console.log(props);
+        return props.tags.map((tag)=>{
+           return tag.file_id.map((id)=>{
+                return (
+                    <ForceGraphLink link={{ source: "File " + id, target: tag.tag_name }} stroke="red" stroke-width="5" />
+            )})
+        })
+    }
+
+    render() {
+      return (
+        <InteractiveForceGraph 
+            zoom
+            labelAttr="data"
+            simulationOptions={{
+                height: 800,
+                width: 1200,
+                animate: true,
+                strength: {
+                    charge: -4000
+                }
+        }}>
+            {this.createTagNodes(this.props)}
+            {this.createFileNodes(this.props)}
+            {this.createLinks(this.props)}
+        </InteractiveForceGraph>
+      )
+    }
+  }
