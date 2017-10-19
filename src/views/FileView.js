b7b0c5a8fe1b296445e3d9b0f88f2820fe11d4ec
Nick Nauert <nicknauert@gmail.com>
2017-10-19 15:13:38 -0400

    began case submission form

--- src/views/FileView.js
+++ src/views/FileView.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import NewFileForm from '../components/NewFileForm'
+import NewCaseForm from '../components/NewCaseForm'
 import "../styles/FileView.css"
 
 export default class FileView extends Component {
   render() {
     return (
       <div className="newFileForm">
-        <NewFileForm />
+        {/* <NewFileForm /> */}
+        <NewCaseForm />
       </div>
     )
   }
