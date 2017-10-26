1cf8360c33da27b00df83dbf34285e6bc29eefde
Ryan Kelley <rtkelley@gmail.com>
2017-10-26 12:41:33 -0400

    bluh

--- src/store/index.js
+++ src/store/index.js
@@ -4,5 +4,5 @@ import thunk from "redux-thunk";
 import combineReducers from "./reducers";
 
 export default function configureStore(initialState) {
-  return createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), initialState, applyMiddleware(thunk));
+  return createStore(combineReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), initialState, applyMiddleware(thunk));
 }
