aa59e61a688751178cfc325639ec69643c9c8fa4
Ryan Kelley <rtkelley@gmail.com>
2017-10-26 13:45:07 -0400

    it's broke

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
-import { createStore, applyMiddleware } from "redux";
+
+import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 
 import combineReducers from "./reducers";
@@ -7,7 +8,10 @@ export default function configureStore(initialState) {
   return createStore(
     reducers,
     initialState,
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    compose(
+      applyMiddleware(thunk),
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
   );
 }
