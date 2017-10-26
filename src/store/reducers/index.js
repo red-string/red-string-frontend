22d84fb9aab255d78e53f574248294e921746c87
Stephen Boynton <boyntonsc@gmail.com>
2017-10-26 10:23:56 -0400

    I don't know what I changed

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,63 +1,71 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from "redux";
+import {
+  getAllCases,
+  getAllFilesFromCase,
+  getAllTagsFromFile,
+  getAllTagsFromCase,
+  getTagsThatShareFiles,
+  getFileById
+} from "../services.js";
 import {} from "../actions/index.js";
 
 const initialState = {
-    cases: [],
-    activeCase: "",
-    activeFile: "",
-    caseFiles: [],
-    caseTags: [],
-    parentNode: {},
-    childNodes: [],
-    previousParent: {},
-    previousChildren: []
-}
+  cases: [],
+  activeCase: "",
+  activeFile: "",
+  caseFiles: [],
+  caseTags: [],
+  parentNode: {},
+  childNodes: [],
+  previousParent: {},
+  previousChildren: []
+};
 
-const reducers = function( state = initialState, action ){
-    switch (action.type){
-        case GET_ALL_CASES:
-            return getAllCases().then(retrievedCases => {
-                return Object.assign({}, state, {
-                    cases: retrievedCases
-                }) 
-            })    
-        break;
+const reducers = function(state = initialState, action) {
+  switch (action.type) {
+    case GET_ALL_CASES:
+      return getAllCases().then(retrievedCases => {
+        return Object.assign({}, state, {
+          cases: retrievedCases
+        });
+      });
+      break;
 
-        case OPEN_CASE:
-            return getAllFilesFromCase(action.payload).then(files => {
-                getAllTagsFromCase(action.payload).then(tags => {
-                    return Object.assign({}, state, {
-                        activeCase: action.payload,
-                        caseFiles: files,
-                        caseTags: tags,
-                        displayUpload: false
-                    })
-                })
-            })
-        break;
+    case OPEN_CASE:
+      return getAllFilesFromCase(action.payload).then(files => {
+        getAllTagsFromCase(action.payload).then(tags => {
+          return Object.assign({}, state, {
+            activeCase: action.payload,
+            caseFiles: files,
+            caseTags: tags,
+            displayUpload: false
+          });
+        });
+      });
+      break;
 
-        case REFRESH_FILE_LIST:
-            return getAllFilesFromCase(state.activeCase).then(files => {
-                getAllTagsFromCase(state.activeCase).then(tags => {
-                    return Object.assign({}, state, {
-                        caseFiles: files,
-                        caseTags: tags
-                    })
-                })
-            })
-        break;
+    case REFRESH_FILE_LIST:
+      return getAllFilesFromCase(state.activeCase).then(files => {
+        getAllTagsFromCase(state.activeCase).then(tags => {
+          return Object.assign({}, state, {
+            caseFiles: files,
+            caseTags: tags
+          });
+        });
+      });
+      break;
 
-        case SET_PARENT_AND_CHILD_NODES:
-            return getTagsThatShareFiles( case_id, file_id ).then( file => {
-                return Object.assign({}, state, {
-                    parentNode: file,
-                    childNodes: file.tags
-                    //MAKE THIS APPLY PREVIOUS NODES
-                })
-            })
-        break;
+    case SET_PARENT_AND_CHILD_NODES:
+      return getTagsThatShareFiles(case_id, file_id).then(file => {
+        return Object.assign({}, state, {
+          parentNode: file,
+          childNodes: file.tags
+          //MAKE THIS APPLY PREVIOUS NODES
+        });
+      });
+      break;
 
-///////////////////THESE ARENT NEEDED FOR THE MOMENT/////////////////
+    ///////////////////THESE ARENT NEEDED FOR THE MOMENT/////////////////
     //     case CREATE_GRAPH:
     //         return Object.assign({}, state, {
     //             cases: action.payload
@@ -69,5 +77,5 @@ const reducers = function( state = initialState, action ){
     //         cases: action.payload
     //     })
     // break;
-    }
-} 
+  }
+};
